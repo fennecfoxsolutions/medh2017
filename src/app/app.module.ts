08565f5072802d1f2ebc6da2b677c8d0a1f426df
Erik Cavalcanti Correa <Erik Cavalcanti Correa>
2017-03-07 17:11:10 -0300

    seções do site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SwiperConfigInterface, SwiperModule } from 'angular2-swiper-wrapper';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+
+const SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  keyboardControl: false,
+  mousewheelControl: false
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SwiperModule.forRoot(SWIPER_CONFIG)
   ],
   providers: [],
   bootstrap: [AppComponent]
